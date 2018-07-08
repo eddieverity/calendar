75cef80210c2f77055f685e2385c41424d661524
Edward Verity <eddieverity@gmail.com>
2018-07-08 16:02:01 -0500

    more edits to appointments, preparing for modals in next push

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -18,113 +18,76 @@ import { CSSTransition, TransitionGroup} from 'react-transition-group';
 import uuid from 'uuid';
 
 class Calendar extends Component {
+
+// Set up state for hardcoded test variables until user login with class-month schema enabled
   state = {
-
     month: [
-      { id: uuid(), day:1, appointment:[]},
-      { id: uuid(), day:2, appointment:[]},
-      { id: uuid(), day:3, appointment:[]},
-      { id: uuid(), day:4, appointment:[]},
-      { id: uuid(), day:5, appointment:[]},
-      { id: uuid(), day:6, appointment:[]},
-      { id: uuid(), day:7, appointment:[]},
-      { id: uuid(), day:8, appointment:[]},
-      { id: uuid(), day:9, appointment:[]},
-      { id: uuid(), day:10, appointment:[]},
-      { id: uuid(), day:11, appointment:[]},
-      { id: uuid(), day:12, appointment:[]},
-      { id: uuid(), day:13, appointment:[]},
-      { id: uuid(), day:14, appointment:[]},
-      { id: uuid(), day:15, appointment:[]},
-      { id: uuid(), day:16, appointment:[]},
-      { id: uuid(), day:17, appointment:[]},
-      { id: uuid(), day:18, appointment:[]},
-      { id: uuid(), day:19, appointment:[]},
-      { id: uuid(), day:20, appointment:[]},
-      { id: uuid(), day:21, appointment:[]},
-      { id: uuid(), day:22, appointment:[]},
-      { id: uuid(), day:23, appointment:[]},
-      { id: uuid(), day:24, appointment:[]},
-      { id: uuid(), day:25, appointment:[]},
-      { id: uuid(), day:26, appointment:[]},
-      { id: uuid(), day:27, appointment:[]},
-      { id: uuid(), day:28, appointment:[]},
-      { id: uuid(), day:29, appointment:[]},
-      { id: uuid(), day:30, appointment:[]},
-      { id: uuid(), day:31, appointment:[]},
-      { id: uuid(), day:1, appointment:[]}
-
+      { id: uuid(), day:1, appointment:[{title:"zxcv", description:"", time:""}]},
+      { id: uuid(), day:2, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:3, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:4, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:5, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:6, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:7, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:8, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:9, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:10, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:11, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:12, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:13, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:14, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:15, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:16, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:17, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:18, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:19, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:20, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:21, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:22, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:23, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:24, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:25, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:26, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:27, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:28, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:29, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:30, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:31, appointment:[{title:"second", description:"baseball", time:"2-4"}]},
+      { id: uuid(), day:1, appointment:[{title:"second", description:"baseball", time:"2-4"}]}
     ]
   }
 
-
   render() {
-    const { items } = this.state;
-    var month = [
-      {day:1,appointment:null},
-      {day:2,appointment:null},
-      {day:3,appointment:null},
-      {day:4,appointment:null},
-      {day:5,appointment:null},
-      {day:6,appointment:null},
-      {day:7,appointment:null},
-      {day:8,appointment:null},
-      {day:9,appointment:null},
-      {day:10,appointment:null},
-      {day:11,appointment:null},
-      {day:12,appointment:null},
-      {day:13,appointment:null},
-      {day:14,appointment:null},
-      {day:15,appointment:null},
-      {day:16,appointment:null},
-      {day:17,appointment:null},
-      {day:18,appointment:null},
-      {day:19,appointment:null},
-      {day:20,appointment:null},
-      {day:21,appointment:null},
-      {day:22,appointment:null},
-      {day:23,appointment:null},
-      {day:24,appointment:null},
-      {day:25,appointment:null},
-      {day:26,appointment:null},
-      {day:27,appointment:null},
-      {day:28,appointment:null},
-      {day:29,appointment:null},
-      {day:30,appointment:null},
-      {day:31,appointment:null}
-    ]
-
+    // sets month to this.state for page manipulation later when appts added/deleted
+    const { month } = this.state;
+    console.log(month)
+    // iterates over month object to display days/add button/pending appointments
     var buttons = month.map(function(day){
-      return <div class="square">           <Button
-            color="light"
-            size="sm"
-
-            style={{ marginBottom: '2rem' }}
-            onClick={() => {
-              const appt = prompt('Enter Appointment Description');
-              if (appt) {
-                this.setState(state => ({
-                  items: [...state.items, { id: uuid(), appt }]
-                }));
-              }
-            }}
-          >
-            {day.day} <span class="appointment">Add Appointment</span>
-          </Button>
-
-
+      return <div class="square">           
+        {day.day}
+        <Button
+          color="light"
+          size="sm"
+          style={{ marginBottom: '2rem' }}
+          onClick={() => {
+            const appt = prompt('Enter Appointment Description');
+            if (appt) {
+              this.setState(state => ({
+                appt: [...state.day.appointment, { id: uuid(), appt }]
+              }));
+            }
+          }}
+        >
+        <span class="appointment">Add Appointment</span>
+        </Button>   
+        <div class="appointmentDisplay">{ day.appointment[0].title }{" "}{day.appointment[0].description}{" "}{day.appointment[0].time}</div>
 {/*
 
-*/}
-
-   
+*/}   
       </div>;
-    
-
-
-
     })
-      return <div class="grid"><div class="month"><b>January 2018</b></div>
+
+    return <div class="grid"><div class="month"><b>January 2018</b></div>
       <div class="grid">
         <div class="title">Sunday</div>
         <div class="title">Monday</div>
